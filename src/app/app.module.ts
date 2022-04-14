f86067ea0d8d50c8a28477eb9a47301ce5494f6f
arip995 <arindampanda2002@gmail.com>
2022-04-14 13:27:52 +0530

    added search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { pipe,switchMap,BehaviorSubject, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ProfileData } from 'src/assets/models/Profile.type';
 
 @Component({
   selector: 'app-profile',
@@ -6,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  profileData!: ProfileData;
+  private refreshToken: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  profileData$: any;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {
+    this.profileData$ = this.refreshToken.pipe(
+      switchMap(() => this.http.get(`https://api.github.com/users/arip995`).pipe(
+        tap((res:any)=>{
+          this.profileData = res;
+        })
+      ))
+    )
+  }
 
   ngOnInit(): void {
   }
